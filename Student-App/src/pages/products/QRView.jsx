05c2dc7cdbb4ecca12de655dce3de6754a394841
dmsxo <67727257+dmsxo@users.noreply.github.com>
2025-10-08 18:00:06 +0900

    make Stat View Ui

--- Student-App/src/pages/products/QRView.jsx
+++ Student-App/src/pages/products/QRView.jsx
@@ -7,6 +7,7 @@ function QRView() {
   return (
     <div className="h-full min-w-fit bg-gray-50 p-4 space-y-3">
       <h1 className="font-black text-gray-900 text-xl mb-4">출석 체크</h1>
+      <div className="flex flex-col items-center justify-center space-y-4"></div>
     </div>
   );
 }
