181c24868b58ff58406e5fc264a44ae36c6f386c
dmsxo <67727257+dmsxo@users.noreply.github.com>
2025-10-08 12:17:15 +0900

    test commit

--- Student-App/src/pages/products/StatView.jsx
+++ Student-App/src/pages/products/StatView.jsx
@@ -5,6 +5,7 @@ function StatView() {
   return (
     <div className="h-full bg-gray-50 min-w-fit p-4 space-y-3">
       <h1 className="font-semibold text-gray-900 text-xl mb-4">출석 현황</h1>
+      <p>test</p>
     </div>
   );
 }
