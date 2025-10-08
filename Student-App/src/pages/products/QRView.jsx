fa22041348c4fe64da715449da47fff857724eb8
dmsxo <67727257+dmsxo@users.noreply.github.com>
2025-10-08 19:33:26 +0900

    QR View test

--- Student-App/src/pages/products/QRView.jsx
+++ Student-App/src/pages/products/QRView.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 import { QRCodeSVG } from 'qrcode.react';
 import { useState, useRef } from 'react';
 import { User, Settings, Bell, Mail } from 'lucide-react';
+import TicketBackground from './test';
 
 function QRView() {
   return (
-    <div className="h-full min-w-fit bg-gray-50 p-4 space-y-3">
-      <h1 className="font-black text-gray-900 text-xl mb-4">출석 체크</h1>
-      <div className="flex flex-col items-center justify-center space-y-4"></div>
+    <div className="bg-gray-100 h-full">
+      <div className="flex flex-col min-w-fit max-w-3xl p-4 space-y-3 ml-auto mr-auto">
+        <h1 className="font-black text-gray-900 text-xl mb-4">출석 체크</h1>
+
+        <TicketBackground
+          notchRatioY={0.3}
+          className="flex flex-col w-11/12 min-w-3xs max-w-lg ml-auto mr-auto"
+        >
+          <div className="flex-1 p-6">
+            <h1 className="font-black text-2xl">학습 티켓</h1>
+          </div>
+          <div className="w-full aspect-square p-16 mb-8">
+            <QRCodeSVG value="" className="size-full" />
+          </div>
+        </TicketBackground>
+      </div>
     </div>
   );
 }
