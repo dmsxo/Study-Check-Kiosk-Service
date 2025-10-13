7c700bf3f35b0d17f969c3fe52cc61e0d20961f9
dmsxo <67727257+dmsxo@users.noreply.github.com>
2025-10-13 23:21:22 +0900

    KioskApp build (electron)

---
+++ Kiosk-App/src/renderer/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col justify-center">
+      <h1 className="text-center font-bold text-3xl text-lime-500">I'm Kiosk!</h1>
+    </div>
+  )
+}
+
+export default App
