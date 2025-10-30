874259c3ddf4259b3233d07e9f58db90012496ca
dmsxo <67727257+dmsxo@users.noreply.github.com>
2025-10-31 07:19:06 +0900

    checkin api 연결

---
+++ Kiosk-App/src/renderer/api/checkin.js
@@ -0,0 +1,22 @@
+import { axios } from 'axios';
+
+const address = "http://localhost:3000";
+
+export async function getCode(){
+  const response = await axios.post(`${API_BASE}/auth/checkin/issue`, {
+      mode: "kiosk:0", // or "kiosk"
+    });
+    return response.data; // { code, expiresIn }
+}
+
+export async function verifyCode(code){
+  try {
+    const response = await axios.post(`${API_BASE}/auth/checkin/verify`, {
+      code: code
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Verify failed", error);
+    throw error;
+  }
+}
