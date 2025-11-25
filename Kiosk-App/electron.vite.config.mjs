4ea6ab3ca4aec9c5ad921da321c23088554c631a
dmsxo <hwangeuntea@gmail.com>
2025-11-25 20:14:43 +0900

    aaaa

--- Kiosk-App/electron.vite.config.mjs
+++ Kiosk-App/electron.vite.config.mjs
@@ -11,12 +11,6 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
-    build: {
-      emptyOutDir: true,
-      // base 경로가 잘못 설정되어 있으면 안 됩니다.
-      base: './', // 상대 경로로 설정되어 있는지 확인 (기본적으로는 './'가 맞습니다)
-      outDir: 'out/renderer', // 출력이 올바른 위치로 가는지 확인
-    },
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
